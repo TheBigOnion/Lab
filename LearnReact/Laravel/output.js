0e0eeee9f4172f4a6eb2a0cb474936c464a90ad5
Michael Poznecki <michael@thebigonion.com>
2025-09-10 09:51:06 -0500

    first commit

---
+++ LearnReact/Laravel/output.js
@@ -0,0 +1,5 @@
+export default function App() {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, "Happy", ' ', new Date().toLocaleDateString('en-US', {
+    weekday: 'long'
+  }), "!"), /*#__PURE__*/React.createElement("p", null, "I hope you have a ", /*#__PURE__*/React.createElement("em", null, "great"), " day!"));
+}
---
+++ LearnReact/Udemy/index.js
@@ -0,0 +1,5 @@
+const number = 3;
+
+const message = number % 2 === 0 ? 'Even Number' : 'Odd Number';
+
+console.log(message);
